2bfe93e215d7a67e54fde065f75d61fb8effdd3a
Nicolas Vanhoren <nicolas.vanhoren.01@gmail.com>
2013-06-12 15:47:22 +0200

    Added .py files to the file exclusion list

--- pygreen.py
+++ pygreen.py
@@ -59,7 +59,7 @@ class PyGreen:
         # A list of regular expression. Files whose the name match
         # one of those regular expressions will not be outputed when generating
         # a static version of the web site
-        self.file_exclusion = [r".*\.mako", r"(^|.*\/)\..*"]
+        self.file_exclusion = [r".*\.mako", r".*\.py", r"(^|.*\/)\..*"]
         def base_lister():
             files = []
             for dirpath, dirnames, filenames in os.walk(self.folder):
