218be342cd937bceb3c5c02c3ec1598b4a940360
Nicolas Vanhoren <n.vanhoren@evs.com>
2018-05-22 12:28:33 +0200

    wip

--- pygreen.py
+++ pygreen.py
@@ -1,28 +1,6 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
-# PyGreen
-# Copyright (c) 2013, Nicolas Vanhoren
-# 
-# Released under the MIT license
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to use,
-# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-# Software, and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-# AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-from __future__ import unicode_literals, print_function
+from __future__ import division, absolute_import, print_function, unicode_literals
 
 import flask
 import os.path
@@ -66,7 +44,7 @@ class PyGreen:
                 if re.match(ex,path):
                     return False
             return True
-            
+
         def base_lister():
             files = []
             for dirpath, dirnames, filenames in os.walk(self.folder):
@@ -124,7 +102,7 @@ class PyGreen:
 
     def gen_static(self, output_folder):
         """
-        Generates a complete static version of the web site. It will stored in 
+        Generates a complete static version of the web site. It will stored in
         output_folder.
         """
         files = []
