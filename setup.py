66e7df5717a71d9cd9fba041d3cff5b9f51fa180
Nicolas Vanhoren <n.vanhoren@evs.com>
2018-05-22 17:03:05 +0200

    Modified homepage in setup.py

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/nicolas-van/pygreen',  # Optional
+    url='https://nicolas-van.github.io/pygreen/',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
